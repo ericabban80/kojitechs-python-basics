91165d8ad95b03e488e6c8ead8f383867be25b7d
ericabban <ericabban80@gmail.com>
2022-05-11 23:20:20 -0400

    UPDATES

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ first_name = "Eric"
 last_name = "abban"
 print (f"every {age} years old is {first_name} {last_name}")
 
+age = 20
+name = "John Smith"
+print (f"we check in patient named {name} who is {age} years old")
